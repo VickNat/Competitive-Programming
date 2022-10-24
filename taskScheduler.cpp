48f034bf984c5470ec153809d9bb450ecf745a5b
NatiVick <114672453+VickNat@users.noreply.github.com>
2022-10-24 23:42:30 +0300

    Create taskScheduler.cpp

---
+++ taskScheduler.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int>mp(26,0);
+        for(auto i :tasks)
+        {
+            mp[i-'A']++;
+        }
+        sort(mp.begin(),mp.end());
+        int maxFreq = mp[25]-1;
+        int idleSlot = maxFreq*n;
+        
+        for(int i=24;i>=0;i--)
+        {
+            idleSlot-=min(mp[i],maxFreq);
+        }   
+        return idleSlot>0?idleSlot+tasks.size():tasks.size();
+    }
+};
