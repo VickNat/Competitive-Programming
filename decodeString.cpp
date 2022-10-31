a449b83d85e427fdbc0057822df682730364bec7
NatiVick <114672453+VickNat@users.noreply.github.com>
2022-10-31 10:37:20 +0300

    Create decodeString.cpp

---
+++ decodeString.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string repeat(string& s, int times){
+        for(int n=s.size(); --times; s+=s.substr(0, n));
+        return s;
+    }
+    string decodeString(string& s) {
+        stack<pair<string, string>> stk;
+        string ans="", n="";
+        for(auto c: s){
+            if(isalpha(c)) ans+=c;
+            else if(isdigit(c)) n+=c;
+            else if(c=='[') stk.push({move(n), move(ans)});
+            else{
+                auto[cnt, prev] = stk.top();
+                stk.pop();
+                ans=prev+repeat(ans, stoi(cnt));
+            }
+        }
+        return ans;
+    }
+};
