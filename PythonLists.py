89f521210db6c9504ae0f268392c57bce694186e
NatiVick <114672453+VickNat@users.noreply.github.com>
2022-12-27 18:45:45 +0300

    Create PythonLists.py

---
+++ PythonLists.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    N = int(input())
+    list = []
+    commands = []
+    
+    for index in range(0, N):
+        commands.append(input())
+    
+    for cmd in commands:
+        if cmd == "print":
+            print(list)
+        else:
+            cmdArgs = cmd.split()
+            operation_args = ','.join(cmdArgs[1:])
+            exec('list.' + cmdArgs[0] + '(' + operation_args + ')')
+    
